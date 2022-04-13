edbf906dd3a13439ecc792f9fe0c4600a592ca1a
Denzyl Dick <denzyl@live.nl>
2022-04-13 18:42:48 +0200

    Use clap for the arguments and added detach argument.

--- src/main.rs
+++ src/main.rs
@@ -8,35 +8,36 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 use daemonize::Daemonize;
+use clap::Parser;
 
 mod config;
 mod server;
 
 fn main() {
-    start();
-    println!("Started");
-    return;
-    let stdout = std::fs::File::create("/tmp/mserver.out").unwrap();
-    let stderr = std::fs::File::create("/tmp/mserver.err").unwrap();
-
-    let deamon = Daemonize::new()
-        .pid_file("/tmp/mserver.pid")
-        .chown_pid_file(false)
-        .working_directory("/tmp")
-        .user("nobody")
-        .group("daemon")
-        .umask(0o777)
-        .stdout(stdout)
-        .stderr(stderr)
-        .exit_action(|| println!(""))
-        .privileged_action(|| println!(""));
-
-    match deamon.start() {
-        Ok(_) => {
-            start();
-        }
-        Err(e) => {
-            println!("Error, {}", e)
+    let args = Args::parse();
+    if args.detach == false {
+        println!("Started");
+        start();
+    } else {
+        let stdout = std::fs::File::create("./mserver.out").unwrap();
+        let stderr = std::fs::File::create("./mserver.err").unwrap();
+        let deamon = Daemonize::new()
+            .pid_file("mserver.pid")
+            .chown_pid_file(false)
+            .working_directory("./")
+            .umask(0o777)
+            .stdout(stdout)
+            .stderr(stderr)
+            .exit_action(|| println!(""))
+            .privileged_action(|| println!(""));
+        match deamon.start() {
+            Ok(_) => {
+                println!("Starting");
+                start();
+            }
+            Err(e) => {
+                println!("Error, {}", e)
+            }
         }
     }
 }
@@ -46,4 +47,12 @@ fn start() {
     println!("Listening for incoming connection on {}:{}", config.host, config.port);
     let mut routes = Routes::new();
     routes.listen_and_serve(config);
+}
+
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    detach: bool,
 }
